b0ee23e91b14fe451e1a6c92bdfba7d9f618db3e
Amany-Hussein <eng.amanyhu@gmail.com>
2026-05-28 19:28:59 +0300

    Add project files.

---
+++ GymProject/Context/GymDbContext.cs
@@ -0,0 +1,17 @@
+﻿using GymProject.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GymProject.Context
+{
+    public class GymDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer
+            ("Server = . ; DataBase = GymDb , Trusted_Connection = true ; TrustedServerCertificate = true");
+        }
+
+        public DbSet<Plan> Plans { get; set; }
+
+    }
+}
---
+++ GymProject/FluentConfigration/PlanConfig.cs
@@ -0,0 +1,14 @@
+﻿using GymProject.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GymProject.FluentConfigration
+{
+    public class PlanConfig : IEntityTypeConfiguration<Plan>
+    {
+        public void Configure(EntityTypeBuilder<Plan> builder)
+        {
+
+        }
+    }
+}
